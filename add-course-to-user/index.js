b8ce8171c14671e54f2c87215bf6d4b7415b8867
Xiao_Lin <Xiao_Lin@compal.com>
2021-09-10 14:21:47 +0800

    feat: add handle failtmp process

--- add-course-to-user/index.js
+++ add-course-to-user/index.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const fs = require('fs-extra');
 
-const arr = require('./proc/exl.json')
-const arr2 = require('./proc/db.json')
+const arr = require('./proc/exl.json');
+const arr2 = require('./proc/db.json');
+
+function successFormat(dbUser, excelUser) {
+  return `${dbUser._id},${dbUser.name || dbUser.nickname},${excelUser.name},${dbUser.account.toLowerCase()},${excelUser.account.toLowerCase()}`
+}
 
 async function filtering() {
   const res = []
@@ -19,7 +23,7 @@ async function filtering() {
       continue
     }
     if (b.length > 1) {
-      console.log(`${a.name} length ${b.length}`)
+      // console.log(`${a.name} length ${b.length}`)
       failAry = failAry.concat(b)
       duplicateCnt += b.length
       continue
@@ -27,7 +31,7 @@ async function filtering() {
     const [bb] = b;
     if (bb.account.toLowerCase().includes(a.account.toLowerCase())) {
       if (gAccounts.has(bb._id)) {
-        console.log(`Duplicated db id ${bb._id}`)
+        // console.log(`Duplicated db id ${bb._id}`)
         continue
       }
       const str = `${bb._id},${bb.name || bb.nickname},${a.name},${bb.account.toLowerCase()},${a.account.toLowerCase()}`
@@ -39,16 +43,65 @@ async function filtering() {
     res.push(ss)
   })
 
-  console.log(`not found ${notFountCnt}`)
-  console.log(`duplicate cnt ${duplicateCnt}`)
-  
-  await fs.writeFile(path.resolve(__dirname, './done/tmp.csv'), res.join('\n'));
-  await fs.writeFile(path.resolve(__dirname, './done/failTmp.json'), JSON.stringify(failAry, null, 4));
-  await fs.writeFile(path.resolve(__dirname, './done/notFound.json'), JSON.stringify(notFoundAry, null, 4));
+  // console.log(`not found ${notFountCnt}`);
+  // console.log(`duplicate cnt ${duplicateCnt}`);
+
+  const failTmpUserMap = new Map()
+  for (const failTmpUser of failAry) { failTmpUserMap.set(failTmpUser._id, failTmpUser) }
+  const uniqFailTmpUsers = Array.from(failTmpUserMap.values());
+
+  return {
+    tmp: res,
+    failTmp: uniqFailTmpUsers,
+    notFound: notFoundAry
+  }
+}
+
+async function filterFailTmp(failTmpUsers) {
+  const tmps = [];
+  const notFound = [];
+
+  for (const exlUser of arr) {
+    const findUsers = failTmpUsers
+      .filter(failTmpUser => {
+        return exlUser.name === failTmpUser.name
+          && failTmpUser.account.toLowerCase().includes(exlUser.account.toLowerCase())
+      });
+
+    if (findUsers.length > 1) { console.log('findUsers', findUsers) }
+
+    if (findUsers.length === 0) {
+      continue;
+    }
+
+    if (findUsers.length === 1) {
+      tmps.push(successFormat(findUsers[0], exlUser))
+    } else {
+      notFound.push(exlUser);
+    }
+  }
+
+  const tmpMap = new Map()
+  for (const tmp of tmps) {
+    tmpMap.set(tmp, tmp);
+  }
+  const uniqTmp = Array.from(tmpMap.values());
+
+  return {
+    tmp: uniqTmp,
+    notFound
+  }
 }
 
 async function main() {
-  await filtering()
+  const filteringResult = await filtering();
+  const filterFailTmpResult = await filterFailTmp(filteringResult.failTmp);
+
+  const tmps = filteringResult.tmp.concat(filterFailTmpResult.tmp);
+  const notFound = filteringResult.notFound.concat(filterFailTmpResult.notFound);
+
+  await fs.writeFile(path.resolve(__dirname, './done/tmp.csv'), tmps.join('\n'));
+  await fs.writeFile(path.resolve(__dirname, './done/notFound.json'), JSON.stringify(notFound, null, 4));
 }
 
 main().catch(ex => console.log(ex));
