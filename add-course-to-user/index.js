382dc57b4fc8babe47859a9c708c82e8a23dddbf
Xiao_Lin <Xiao_Lin@compal.com>
2021-09-22 15:34:15 +0800

    feat: add filter already exist study

--- add-course-to-user/index.js
+++ add-course-to-user/index.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const fs = require('fs-extra');
 
-const arr = require('./proc/exl.json');
-const arr2 = require('./proc/db.json');
+const exlArr = require('./proc/exl.json');
+const dbArr = require('./proc/db.json');
+const studies = require('./proc/study.json')
 
 function successFormat(dbUser, excelUser) {
   return `${dbUser._id},${dbUser.name || dbUser.nickname},${excelUser.name},${dbUser.account.toLowerCase()},${excelUser.account.toLowerCase()}`
@@ -10,7 +11,7 @@ function successFormat(dbUser, excelUser) {
 
 function uniqExl() {
   const uniqMap = new Map();
-  for (const ele of arr) {
+  for (const ele of exlArr) {
     uniqMap.set(`${ele.name}${ele.account}${ele.phone}`, ele);
   }
 
@@ -22,14 +23,16 @@ async function filtering() {
   let failAry = []
   const notFoundAry = []
   const gAccounts = new Map();
+  // exl 搜尋 db 多筆資料
+  const duplicateArr = []
   let [notFountCnt, duplicateCnt] = [0, 0, 0]
 
-  const exlAry = uniqExl();
-  console.log('exlAry', exlAry.length);
+  const uniqExlAry = uniqExl();
+  console.log('exlAry', uniqExlAry.length);
   let duplicateDBCount = 0
 
-  for (const a of exlAry) {
-    const b = arr2.filter(x => (x.name === a.name || x.nickname === a.name));
+  for (const a of uniqExlAry) {
+    const b = dbArr.filter(x => (x.name === a.name || x.nickname === a.name));
 
     if (b.length === 0) {
       notFoundAry.push(a);
@@ -39,6 +42,7 @@ async function filtering() {
     if (b.length > 1) {
       // console.log(`${a.name} length ${b.length}`)
       failAry = failAry.concat(b)
+      duplicateArr.push(a)
       // duplicateCnt += b.length
       duplicateCnt++
       continue
@@ -62,69 +66,128 @@ async function filtering() {
     res.push(ss)
   })
 
+  // exl 重複名字，db 只有一筆
   console.log('duplicateDBCount', duplicateDBCount);
+  // exl 一筆資料，db搜尋出多筆
   console.log('duplicateCnt', duplicateCnt);
 
   const failTmpUserMap = new Map()
   for (const failTmpUser of failAry) { failTmpUserMap.set(failTmpUser._id, failTmpUser) }
   const uniqFailTmpUsers = Array.from(failTmpUserMap.values());
 
+  // db 與 exl 無法比對資料
   console.log('notFoundAry', notFoundAry.length);
+  // 
   console.log('first res', res.length);
   return {
     tmp: res,
     failTmp: uniqFailTmpUsers,
-    notFound: notFoundAry
+    notFound: notFoundAry,
+    duplicateArr
   }
 }
 
-async function filterFailTmp(failTmpUsers) {
+async function filterFailTmp(failTmpUsers, duplicateArr) {
+  console.log('\nround 2-------------------------------------------------------');
   const tmps = [];
   const notFound = [];
 
-  for (const exlUser of arr) {
+  let zeroLength = 0;
+  let mulLength = 0;
+  let uniqLength = 0;
+  for (const exlUser of duplicateArr) {
     const findUsers = failTmpUsers
       .filter(failTmpUser => {
         return exlUser.name === failTmpUser.name
           && failTmpUser.account.toLowerCase().includes(exlUser.account.toLowerCase())
       });
 
-    if (findUsers.length > 1) { console.log('findUsers', findUsers) }
+    if (findUsers.length > 1) {
+      mulLength++
+      notFound.push(exlUser);
+      continue;
+    }
 
     if (findUsers.length === 0) {
+      zeroLength++
+      notFound.push(exlUser);
       continue;
     }
 
     if (findUsers.length === 1) {
       tmps.push(successFormat(findUsers[0], exlUser))
-    } else {
-      notFound.push(exlUser);
     }
   }
 
   const tmpMap = new Map()
   for (const tmp of tmps) {
+    if (tmpMap.has(tmp)) {
+      uniqLength++
+    }
+
     tmpMap.set(tmp, tmp);
   }
   const uniqTmp = Array.from(tmpMap.values());
 
+  // exl 對 db 數等於 0 次數
+  console.log('zeroLength', zeroLength);
+  // exl 對 db 數大於 1 次數
+  console.log('mulLength', mulLength);
+  // 成功uniq後數
+  console.log('uniqLength', uniqLength);
 
-  console.log('second uniqTmp', uniqTmp.length)
   console.log('second notFound', notFound.length)
+  // 最後成功數
+  console.log('second res', uniqTmp.length)
   return {
     tmp: uniqTmp,
     notFound
   }
 }
 
+function filterStudy(tmps) {
+  console.log('\nround 3-------------------------------------------------------');
+  if (!studies.length) {
+    return tmps;
+  }
+
+  const yetJoinUsersMap = new Map();
+  const filterUsersMap = new Map();
+  for (const tmp of tmps) {
+    const tmpStrArr = tmp.split(',');
+    const userId = tmpStrArr[0];
+
+    const study = studies.find(study => study.userId === userId);
+    if (!study) {
+      yetJoinUsersMap.set(tmp, tmp);
+    } else {
+      filterUsersMap.set(tmp, tmp);
+    }
+  }
+
+  // 未購課人數
+  console.log('third res', yetJoinUsersMap.size);
+  // 已購課人數
+  console.log('already into study', filterUsersMap.size);
+
+  return {
+    yetJoinUsers: Array.from(yetJoinUsersMap),
+    filterUsers: Array.from(filterUsersMap)
+  };
+}
+
 async function main() {
   const filteringResult = await filtering();
-  const filterFailTmpResult = await filterFailTmp(filteringResult.failTmp);
+  const filterFailTmpResult = await filterFailTmp(filteringResult.failTmp, filteringResult.duplicateArr);
 
-  const tmps = filteringResult.tmp.concat(filterFailTmpResult.tmp);
   const notFound = filteringResult.notFound.concat(filterFailTmpResult.notFound);
+  const tmps = filteringResult.tmp.concat(filterFailTmpResult.tmp);
 
-  await fs.writeFile(path.resolve(__dirname, './done/tmp.csv'), tmps.join('\n'));
+  // study filter
+  const users = filterStudy(tmps)
+
+  await fs.writeFile(path.resolve(__dirname, './done/已購課.csv'), users.filterUsers.join('\n'));
+  await fs.writeFile(path.resolve(__dirname, './done/未購課.csv'), users.yetJoinUsers.join('\n'));
   await fs.writeFile(path.resolve(__dirname, './done/notFound.json'), JSON.stringify(notFound, null, 4));
 }
 
